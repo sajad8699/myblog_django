dd7acda72f678eeb1a7a4ba9143f77d99899ad97
sajad8699 <sajadsd1397@gmail.com>
2020-07-08 20:51:11 +0430

    new

---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+
+# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #login
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
+    return render(request,'accounts/signup.html',{'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # login user
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html',{'form':form})
