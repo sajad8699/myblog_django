bc7b40731db675862dd3ec9bfa8583b1a90ad06a
sajad8699 <sajadsd1397@gmail.com>
2020-07-12 14:07:56 +0430

    css 1

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout
 
 # Create your views here.
 def signup_view(request):
@@ -7,7 +8,9 @@ def signup_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            #login
+            user = form.save()
+            login(request,user)
+            # login
             return redirect('articles:list')
     else:
         form = UserCreationForm()
@@ -18,7 +21,17 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             # login user
-            return redirect('articles:list')
+            user = form.get_user()
+            login(request, user)
+            if 'next' in  request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('articles:list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html',{'form':form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from . import models
+from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 def articles_list(request):
@@ -11,3 +13,17 @@ def article_detail(request, slug):
     # return HttpResponse(slug)
     article = models.Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article':article})
+
+@login_required(login_url = "/accounts/login")
+def create_article(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST,request.FILES)
+        if form.is_valid:
+            instance = form.save(commit = False)
+            instance.author = request.user
+            instance.save()
+            return redirect('articles:list')
+
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/create_article.html',{'form':form})
